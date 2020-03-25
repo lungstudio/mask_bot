34d8ea3c6ed463af5e042da54f118be2452df40b
lungstudio <lung2001@gmail.com>
2020-03-26 01:15:44 +0800

    -fake user agent -add delay

--- mask_track.py
+++ mask_track.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 from dotenv import load_dotenv
 from os.path import join, dirname
 
@@ -10,6 +11,7 @@ MASK_URLS = os.environ['MASK_URLS']
 KEYWORDS_FOR_TRIGGER = os.environ['KEYWORDS_FOR_TRIGGER']
 BOT_API_KEY = os.environ['BOT_API_KEY']
 CHANNEL_NAME = os.environ['CHANNEL_NAME']
+SLEEP_SEC = int(os.environ['SLEEP_SEC'])
 
 def send_alert(url):
     msg = f"有口罩賣啦！{url}"
@@ -18,15 +20,23 @@ def send_alert(url):
 
 # main function for the cloud function to be executed
 def mask_track(request):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
+    }
+
     for url in MASK_URLS.split(','):
-        r = requests.get(url)
+        r = requests.get(url, headers = headers)
 
         status = r.status_code
         is_mask_available = KEYWORDS_FOR_TRIGGER in r.text
         print(f"[mask_track] status: {status}, is_mask_available: {is_mask_available}, url: {url}")
 
+
         if status == 200 and is_mask_available:
             send_alert(url)
+
+        time.sleep(SLEEP_SEC)
 
 if __name__ == '__main__':
     mask_track(None)
